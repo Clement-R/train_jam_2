34cb2c58edcddb23ad12082419b42cbe41a4f3ef
Clement-R <rondeau.clement@gmail.com>
2018-06-09 12:02:15 +0200

    Spawn random minions on start

--- Assets/GameOrchestrator.cs
+++ Assets/GameOrchestrator.cs
@@ -2,13 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOrchestrator : MonoBehaviour {
-
+public class GameOrchestrator : MonoBehaviour
+{
+	public int nbOfMinions;
+	public GameObject minionPrefab;
+	public float rndRadius;
+	public float minionRadiusSize;
+	
 	void Start () {
 		
-	}
-	
-	void Update () {
-		
+		// Spawn minions
+		for (int i = 0; i < nbOfMinions; i++)
+		{
+			for (int j = 0; j < 100; j++)
+			{
+				// Get a random point in a radius around 0,0
+				Vector2 rndPos = Random.insideUnitCircle;
+				rndPos = new Vector2(
+					Random.Range(-rndPos.x * rndRadius * 2f, rndPos.x * rndRadius * 2f),
+					Random.Range(-rndPos.y * rndRadius * 2f, rndPos.y * rndRadius * 2f));
+
+				// Check if can spawn a minion here
+				Collider2D[] cols = Physics2D.OverlapCircleAll(Vector2.zero, minionRadiusSize);
+				if (cols.Length == 0)
+				{
+					// Spawn it	
+					Instantiate(minionPrefab, rndPos, Quaternion.identity);
+					break;
+				}	
+			}
+		}
 	}
 }
--- Assets/MinionBehaviour.cs
+++ Assets/MinionBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MinionBehaviour : MonoBehaviour
 {
 	public float speed = 150f;
+	public float maxSpeed = 300f;
 	
 	private Rigidbody2D _rb2d;
 	private Vector2 _target;
@@ -22,6 +23,12 @@ public class MinionBehaviour : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		_rb2d.velocity = (_target - new Vector2(transform.position.x, transform.position.y)).normalized * speed;
+		// _rb2d.velocity = (_target - new Vector2(transform.position.x, transform.position.y)).normalized * speed;
+		Vector2 desiredVelocity = (_target - new Vector2(transform.position.x, transform.position.y)).normalized * speed;
+		Vector2 steering = desiredVelocity - _rb2d.velocity;
+		steering = Vector2.ClampMagnitude(steering, maxSpeed);
+            
+		_rb2d.AddForce(steering);
+		
 	}
 }
