df2080739eea692a87ce5244a9c08c00c289fde0
Clement-R <rondeau.clement@gmail.com>
2018-06-09 11:42:44 +0200

    Add follow behaviour on minions

---
+++ Assets/MinionBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionBehaviour : MonoBehaviour
+{
+	public float speed = 150f;
+	
+	private Rigidbody2D _rb2d;
+	private Vector2 _target;
+	
+	void Start ()
+	{
+		_rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+	{
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		_target = new Vector2(mousePos.x, mousePos.y);
+	}
+
+	private void FixedUpdate()
+	{
+		_rb2d.velocity = (_target - new Vector2(transform.position.x, transform.position.y)).normalized * speed;
+	}
+}
